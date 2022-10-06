59e79f3fc01e2a6ef7dbd31c5b5266b4c1012afc
jacobgifford22 <90434868+jacobgifford22@users.noreply.github.com>
2022-10-05 19:39:22 -0600

    Meeting Conclusion Commit

--- Wordle.py
+++ Wordle.py
@@ -7,8 +7,10 @@ BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
 
 import random
 
+from pydantic import MissingError
+
 from WordleDictionary import FIVE_LETTER_WORDS
-from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+from WordleGraphics import CORRECT_COLOR, MISSING_COLOR, PRESENT_COLOR, WordleGWindow, N_COLS, N_ROWS
 
 LRandomWord = []
 iRow = 0
@@ -19,17 +21,31 @@ def wordle():
     def enter_action(s):
         global iRow
         global sRandomWord
+        global LRandomWord
         sInWordList = "no"
         sGuessedWord = s.upper()
         LGuessedWord = []
+
+        def changeWordleSquare(LRandomWord):
+            print("Hello " + str(LRandomWord))
+
+            for i in range(len(LGuessedWord)):
+                if LGuessedWord[i].upper() == LRandomWord[i].upper():
+                    gw.set_square_color(iRow, i, CORRECT_COLOR) 
+                elif LGuessedWord[i].upper() in sRandomWord.upper() :  
+                    gw.set_square_color(iRow, i, PRESENT_COLOR)
+                else:
+                    gw.set_square_color(iRow, i, MISSING_COLOR)
 
         for letter in sGuessedWord:
             if letter.strip() != '':
                 LGuessedWord.append(letter.upper())
 
         if iRow == N_ROWS - 1:
+            changeWordleSquare(LRandomWord)
             gw.show_message("Sorry champ you ran out of guesses...")
         elif sRandomWord.upper() == sGuessedWord:
+            changeWordleSquare(LRandomWord)
             gw.show_message("YOU GOT IT BUDDY!!!")
         else:
             for i in range(len(FIVE_LETTER_WORDS)):
@@ -38,11 +54,18 @@ def wordle():
         
             if sInWordList == "yes":
                 gw.show_message("That is a valid word!")
+                changeWordleSquare(LRandomWord)
+
                 iRow += 1
             else:
                 gw.show_message("Not in word list.")
 
             gw.set_current_row(iRow)
+
+            
+
+        
+                    
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
@@ -57,10 +80,6 @@ def wordle():
         for letter in sRandomWord:
             if letter.strip() != '':
                 LRandomWord.append(letter.upper())
-
-        print(LRandomWord)
-        print(sRandomWord)
-        print(LRandomWord[0])
     
     randomWord()
 
